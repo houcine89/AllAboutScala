0d78f212ca99ce06d94f7a27694d51072b8902ab
Houcine GASMI <houcine.gasmi3@docapost.fr>
2018-01-02 14:22:32 +0100

    Tutorial 10 : add logger with SBT.

---
+++ src/main/scala/com/allaoutscala/one/tutorial_10/HelloWorldWithScalaLogging.scala
@@ -0,0 +1,11 @@
+package com.allaoutscala.one.tutorial_10
+
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+  * Created by hgasmi on 02/01/18.
+  */
+object HelloWorldWithScalaLogging extends App with LazyLogging {
+
+  logger.info("Hello world from scala logging.")
+}
