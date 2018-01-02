050ce2b8e810bd97a5a547bf3bd81ad7c2d56d16
Houcine GASMI <houcine.gasmi3@docapost.fr>
2018-01-02 12:18:20 +0100

    Setup IntelliJ scala project & HelloWorld exemples.

---
+++ src/main/scala/com/allaoutscala/one/tutorial_04/HelloWorld.scala
@@ -0,0 +1,9 @@
+package com.allaoutscala.one.tutorial_04
+
+/**
+  * Created by hgasmi on 02/01/18.
+  */
+object HelloWorld extends App{
+
+  print("Hello World from scala!")
+}
---
+++ src/main/scala/com/allaoutscala/one/tutorial_05/HelloWorldMain.scala
@@ -0,0 +1,12 @@
+package com.allaoutscala.one.tutorial_05
+
+/**
+  * Created by hgasmi on 02/01/18.
+  */
+object HelloWorldMain {
+
+  def main(args: Array[String]): Unit = {
+    print("Hello World from main function! ")
+  }
+
+}
