3ce2a02c9d7ffe26443a6314ac38cf03947d7c3e
Houcine GASMI <houcine.gasmi3@docapost.fr>
2018-01-02 12:40:46 +0100

    Tutorial 07 : debug with arguments.

---
+++ src/main/scala/com/allaoutscala/one/tutorial_07/HelloWorldWithArgumentsDebug.scala
@@ -0,0 +1,11 @@
+package com.allaoutscala.one.tutorial_07
+
+/**
+  * Created by hgasmi on 02/01/18.
+  */
+object HelloWorldWithArgumentsDebug extends App{
+  println("Hello World with arguments scala application!")
+
+  println("Command line arguments are: ")
+  println(args.mkString(", "))
+}
