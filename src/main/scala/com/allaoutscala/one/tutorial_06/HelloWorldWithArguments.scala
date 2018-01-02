bdb934558ec24ae18ef4b2e4d6e04fe74bf1b8d7
Houcine GASMI <houcine.gasmi3@docapost.fr>
2018-01-02 12:36:28 +0100

    Tutorial 06 : run with arguments.

---
+++ src/main/scala/com/allaoutscala/one/tutorial_06/HelloWorldWithArguments.scala
@@ -0,0 +1,12 @@
+package com.allaoutscala.one.tutorial_06
+
+/**
+  * Created by hgasmi on 02/01/18.
+  */
+object HelloWorldWithArguments extends App{
+
+ println("Hello World with arguments scala application!")
+
+  println("Command line arguments are: ")
+  println(args.mkString(", "))
+}
